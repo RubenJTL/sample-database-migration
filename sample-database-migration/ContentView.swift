cad13de94342e8188348262538ed09d123f03e95
RubenJTL <ruben@ravn.co>
2022-12-23 12:01:17 -0500

    Initial Commit

---
+++ sample-database-migration/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  sample-database-migration
+//
+//  Created by Ruben Torres on 23/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sample-database-migration/sample_database_migrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_database_migrationApp.swift
+//  sample-database-migration
+//
+//  Created by Ruben Torres on 23/12/22.
+//
+
+import SwiftUI
+
+@main
+struct sample_database_migrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
